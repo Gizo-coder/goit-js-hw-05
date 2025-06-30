76eeb529f0b1854de2c7c33146eb6bf63588eecd
Gizem Demirci <gizdemirci91@gmail.com>
2025-06-30 23:12:15 +0300

    at least

--- js/task-4.js
+++ js/task-4.js
@@ -1,11 +1,12 @@
 // Görev 4. Toplam bakiye
 
 const getTotalBalanceByGender = (users, gender) => {
-  return users.reduce((total, user) => {
-    return user.gender === gender ? total + user.balance : total;
-  }, 0);
+  
+  return users.filter(user => user.gender === gender)
+    .reduce((total, user) => {
+      return total + user.balance
+    }, 0);
 };
-
 
 
 
